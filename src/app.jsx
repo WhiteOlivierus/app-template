ee61da563ab0e05a4dba258d9c5ff11db852f821
WhiteOlivierus <bas.dijkstra@student.hku.nl>
2021-06-12 12:37:22 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Engine from "engine";
+import projectData from "./projectData";
+
+const importAll = (resource) => resource.keys().map(resource);
+
+importAll(require.context("./img", false, /\.(png|jpe?g|gif)$/));
+
+const preProcessor = {
+	...projectData,
+	images: [...projectData.images.map((i) => `/img/${i}`)],
+};
+
+ReactDOM.render(
+	<Engine data={preProcessor} />,
+	document.getElementById("root")
+);
